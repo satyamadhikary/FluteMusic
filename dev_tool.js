4307d2272f4f4516359e9e7b3dc63b871b93361f
Satyam Adhikary <satyamadhikary226@gmail.com>
2024-09-06 20:46:44 +0530

    Custom Audio player

--- dev_tool.js
+++ dev_tool.js
@@ -1,15 +1,15 @@
-document.addEventListener('contextmenu', function(event) {
-    event.preventDefault();
-  });
+// document.addEventListener('contextmenu', function(event) {
+//     event.preventDefault();
+//   });
   
-  document.onkeydown = function(e) {
-    if (e.keyCode == 123) { // 123 is the keycode for F12
-        return false;
-  }
-}
+//   document.onkeydown = function(e) {
+//     if (e.keyCode == 123) { // 123 is the keycode for F12
+//         return false;
+//   }
+// }
 
-document.addEventListener('keydown', function(event) {
-    if (event.ctrlKey && event.shiftKey && event.key === 'I') {
-        event.preventDefault();
-    }
-});
+// document.addEventListener('keydown', function(event) {
+//     if (event.ctrlKey && event.shiftKey && event.key === 'I') {
+//         event.preventDefault();
+//     }
+// });
--- script.js
+++ script.js
@@ -41,47 +41,100 @@ owl.on('mousewheel', function (e) {
 
 
 const items = [
-    {
-      className: 'item1',
-      imageSrc: 'images/EeeHawa.jpeg',
-      audioSrc: 'https://firebasestorage.googleapis.com/v0/b/storage-bucket-575e1.appspot.com/o/music%2Fin-y2mate.com%20-%20E%20Hawa%20%20Meghdol%20X%20Hawa%20Film%20%20Aluminium%20Er%20Dana.mp3?alt=media&token=3724b578-ea7e-45c9-8ada-9dd5db28fca9',
-      title: 'E Hawa'
-    },
-    {
-      className: 'item2',
-      imageSrc: 'images/Ghorgari.png',
-      audioSrc: 'https://firebasestorage.googleapis.com/v0/b/storage-bucket-575e1.appspot.com/o/music%2Fin-y2mate.com%20-%20GhorGari%20%E0%A6%98%E0%A6%B0%E0%A6%97%E0%A6%A1%20%20Album%20Train%20Poka%20%20HIGHWAY.mp3?alt=media&token=5c83592f-9ec2-48ed-b44a-3865a15ae03a',
-      title: 'GhorGari'
-    },
-    {
-      className: 'item3',
-      imageSrc: 'images/Obosthan.jpeg',
-      audioSrc: 'https://firebasestorage.googleapis.com/v0/b/storage-bucket-575e1.appspot.com/o/music%2FObosthan.mp3?alt=media&token=9f0c0914-2ff4-40f5-8545-f23df037532b',
-      title: 'Obosthan'
-    },
-    {
-      className: 'item4',
-      imageSrc: 'images/sakkhi.jpg',
-      audioSrc: 'https://firebasestorage.googleapis.com/v0/b/storage-bucket-575e1.appspot.com/o/music%2FSakkhi.mp3?alt=media&token=c8365f21-07f1-4021-900b-96cf1997d4e2',
-      title: 'Sakkhi'
-    }
-  ];
+  {
+    className: 'item1',
+    imageSrc: 'images/EeeHawa.jpeg',
+    audioSrc: 'https://firebasestorage.googleapis.com/v0/b/storage-bucket-575e1.appspot.com/o/music%2Fin-y2mate.com%20-%20E%20Hawa%20%20Meghdol%20X%20Hawa%20Film%20%20Aluminium%20Er%20Dana.mp3?alt=media&token=3724b578-ea7e-45c9-8ada-9dd5db28fca9',
+    title: 'E Hawa'
+  },
+  {
+    className: 'item2',
+    imageSrc: 'images/Ghorgari.png',
+    audioSrc: 'https://firebasestorage.googleapis.com/v0/b/storage-bucket-575e1.appspot.com/o/music%2Fin-y2mate.com%20-%20GhorGari%20%E0%A6%98%E0%A6%B0%E0%A6%97%E0%A6%A1%20%20Album%20Train%20Poka%20%20HIGHWAY.mp3?alt=media&token=5c83592f-9ec2-48ed-b44a-3865a15ae03a',
+    title: 'GhorGari'
+  },
+  {
+    className: 'item3',
+    imageSrc: 'images/Obosthan.jpeg',
+    audioSrc: 'https://firebasestorage.googleapis.com/v0/b/storage-bucket-575e1.appspot.com/o/music%2FObosthan.mp3?alt=media&token=9f0c0914-2ff4-40f5-8545-f23df037532b',
+    title: 'Obosthan'
+  },
+  {
+    className: 'item4',
+    imageSrc: 'images/sakkhi.jpg',
+    audioSrc: 'https://firebasestorage.googleapis.com/v0/b/storage-bucket-575e1.appspot.com/o/music%2FSakkhi.mp3?alt=media&token=c8365f21-07f1-4021-900b-96cf1997d4e2',
+    title: 'Sakkhi'
+  }
+];
 
-  document.querySelectorAll('.item').forEach(item => {
-    const className = item.getAttribute('data-info');
-    const itemData = items.find(i => i.className === className);
-    
-    if (itemData) {
-      item.innerHTML = `
-        <div class="pad-content">
-          <div class="img-container">
-            <img class="img-fluid play-img change" src="${itemData.imageSrc}" alt="${itemData.title}">
-            <h2>${itemData.title}</h2>
-            <audio id="${itemData.className}" controls controlsList="nodownload noplaybackrate">
-              <source src="${itemData.audioSrc}">
-            </audio>
+document.querySelectorAll('.item').forEach(item => {
+  const className = item.getAttribute('data-info');
+  const itemData = items.find(i => i.className === className);
+
+  if (itemData) {
+    item.innerHTML = `
+      <div class="pad-content">
+        <div class="img-container">
+          <img class="img-fluid play-img change" src="${itemData.imageSrc}" alt="${itemData.title}">
+          <h2>${itemData.title}</h2>
+          <audio id="${itemData.className}" src="${itemData.audioSrc}"></audio>
+          <div class="controls">
+            <button class="play-pause play"></button>
+            <button class="stop" style="display: none;"></button>
+            <input type="range" class="seek-bar" value="0" max="100">
           </div>
         </div>
-      `;
-    }
-  });
+      </div>
+    `;
+
+    // Attach event listeners to the current item
+    const audio = item.querySelector('audio');
+    const playPauseBtn = item.querySelector('.play-pause');
+    const seekBar = item.querySelector('.seek-bar');
+
+    playPauseBtn.addEventListener('click', () => {
+      if (audio.paused) {
+        audio.play();
+        playPauseBtn.classList.remove('play');
+        playPauseBtn.classList.add('pause');
+      } else {
+        audio.pause();
+        playPauseBtn.classList.remove('pause');
+        playPauseBtn.classList.add('play');
+      }
+    });
+
+    audio.addEventListener('timeupdate', () => {
+      seekBar.value = (audio.currentTime / audio.duration) * 100;
+    });
+
+    seekBar.addEventListener('input', () => {
+      audio.currentTime = (seekBar.value / 100) * audio.duration;
+    });
+  }
+});
+
+// const audio = document.getElementById('audio');
+// const playPauseBtn = document.getElementById('play-pause');
+// const stopBtn = document.getElementById('stop');
+// const seekBar = document.getElementById('seek-bar');
+
+// playPauseBtn.addEventListener('click', () => {
+//     if (audio.paused) {
+//         audio.play();
+//         playPauseBtn.classList.remove('play');
+//         playPauseBtn.classList.add('pause');
+//     } else {    
+//         audio.pause();
+//         playPauseBtn.classList.remove('pause');
+//         playPauseBtn.classList.add('play');
+//     }
+// });
+
+// audio.addEventListener('timeupdate', () => {
+//     seekBar.value = (audio.currentTime / audio.duration) * 100;
+// });
+
+// seekBar.addEventListener('input', () => {
+//     audio.currentTime = (seekBar.value / 100) * audio.duration;
+// });
